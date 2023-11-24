38f85862a67e70fa30a29cfb9089c6b2a369f434
Adam Kvasnicka <adam.kvasnicka.dmd@gmail.com>
2023-11-24 13:20:19 +0100

    chore: expose constants

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ export { default as CSSTransition } from './CSSTransition';
 export { default as ReplaceTransition } from './ReplaceTransition';
 export { default as SwitchTransition } from './SwitchTransition';
 export { default as TransitionGroup } from './TransitionGroup';
-export { default as Transition } from './Transition';
+export {
+  default as Transition,
+  ENTERED,
+  ENTERING,
+  EXITED,
+  EXITING,
+  UNMOUNTED,
+} from './Transition';
 export { default as config } from './config';
